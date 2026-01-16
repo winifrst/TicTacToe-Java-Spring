c1de2c94ec8095b101db5cb361f5d38b0a7bc6c3
winifrst <you@example.com>
2026-01-16 09:57:57 +0300

    update build.gradle.kts

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     java
     id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.20" // gradle
+
 }
 
 group = "org.tictactoe"
@@ -17,5 +19,6 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter") // Явное подключение Spring Core
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
