423935b41072b172123f5b8c6f03c645e305ce54
winifrst <you@example.com>
2026-01-24 22:46:47 +0300

    add docker database

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.2.0"
+    id("org.springframework.boot") version "3.2.4"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.20" // gradle
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "org.tictactoe"
 version = "1.0-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_18
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -17,23 +18,29 @@ repositories {
 }
 
 dependencies {
-
-    // Spring Boot Starters
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 //    implementation("org.springframework.boot:spring-boot-starter-web")
 //    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-//     Spring Security для авторизации
+
     implementation("org.springframework.boot:spring-boot-starter-security")
 
     // База данных
-    runtimeOnly("org.postgresql:postgresql")  // runtimeOnly вместо implementation
+    runtimeOnly("org.postgresql:postgresql")
     //    implementation("org.postgresql:postgresql")
 
     implementation("org.springframework.boot:spring-boot-starter") // Явное подключение Spring Core
 
-//    // Тестирование
+//    runtimeOnly("com.h2database:h2")
+
+    // Kotlin поддержка
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
 //    testImplementation("org.springframework.boot:spring-boot-starter-test")
-//    testImplementation("org.springframework.security:spring-security-test") // Для тестов безопасности
+//    testImplementation("org.springframework.security:spring-security-test")
+
+    testImplementation("org.testcontainers:postgresql:1.19.3")
+    testImplementation("org.testcontainers:junit-jupiter:1.19.3")
 }
